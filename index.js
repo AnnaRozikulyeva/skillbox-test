41461da419d65456e7914f3efe67db8c49a2be06
Anna Rozikulyeva <rav4@live.ru>
2025-02-10 16:11:59 +0400

    Module #4

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const currentDateTime = require("./date");
+const fs = require("fs");
+const crypto = require("crypto");
 
-console.log("Today is " + currentDateTime.date + ", the current time is " + currentDateTime.time + ".");
+const path = process.argv[2];
+const hash_path = `${path}.sha256`;
+
+fs.readFile(path, (error, data) => {
+  if (error) {
+    console.error(error);
+    process.exit(100);
+  }
+
+  const hash = crypto.createHash("sha256").update(data).digest("hex");
+
+  fs.readFile(hash_path, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(101);
+    }
+
+    if (hash != data.trim()) {
+      process.exit(102);
+    } else {
+      console.log("ok");
+      process.exit(0);
+    }
+  });
+});
